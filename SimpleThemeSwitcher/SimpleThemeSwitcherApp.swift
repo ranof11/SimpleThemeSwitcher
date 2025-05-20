5aafff12dfa60601670dadcc27fa624140106aa5
ranof11 <rajeshtriadi14@gmail.com>
2025-05-20 11:24:29 +0700

    Initial Commit

---
+++ SimpleThemeSwitcher/SimpleThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleThemeSwitcherApp.swift
+//  SimpleThemeSwitcher
+//
+//  Created by Rajesh Triadi Noftarizal on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
